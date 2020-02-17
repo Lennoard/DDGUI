94a27553385a88010cc6ec732c17fa7424bd6101
lennoard <lennoardrai@gmail.com>
2020-02-16 22:31:09 -0300

    Initial commit

---
+++ app/src/main/java/com/androidvip/ddgui/Extensions.kt
@@ -0,0 +1,41 @@
+package com.androidvip.ddgui
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+fun View.goAway() { this.visibility = View.GONE }
+fun View.hide() { this.visibility = View.INVISIBLE }
+fun View.show() { this.visibility = View.VISIBLE }
+
+fun Context?.toast(messageRes: Int, length: Int = Toast.LENGTH_SHORT) {
+    if (this == null) return
+    toast(getString(messageRes), length)
+}
+
+fun Context?.toast(message: String?, length: Int = Toast.LENGTH_SHORT) {
+    if (message == null || this == null) return
+    val ctx = this
+
+    // Just in case :P
+    GlobalScope.launch(Dispatchers.Main) {
+        Toast.makeText(ctx, message, length).show()
+    }
+}
+
+suspend inline fun Activity?.runSafeOnUiThread(crossinline uiBlock: () -> Unit) {
+    this?.let {
+        if (!it.isFinishing) {
+            withContext(Dispatchers.Main) {
+                runCatching(uiBlock)
+            }
+        }
+    }
+}
